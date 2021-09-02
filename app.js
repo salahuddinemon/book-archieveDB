de598c739f8c63142c19f6329b5509a8a3d04c06
salahuddinemon <emonthex@gmail.com>
2021-09-02 08:14:38 +0600

    cover

--- app.js
+++ app.js
@@ -18,14 +18,16 @@ const displayOpenLibrary = docs => {
     searchResult.textContent = '';
     // data catching
     docs.forEach(doc => {
+        const coverUrl = `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`
+        console.log(coverUrl)
         // console.log(doc.author_name['0']);
         const div = document.createElement('div');
         div.innerHTML = `
         <div class="card h-100">
-            <img src="" class="card-img-top" alt="...">
+            <img src="${coverUrl}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">${doc.author_name}</h5 >
-                <p class="card-text">${doc.title}</p>
+            <h4 class="card-text">${doc.title}</h4>
+            <h6 class="card-title">${doc.author_name}</h6>
                 <p class="card-text">${doc.first_publish_year}</p>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
                 additional content. This content is a little bit longer.</p>
