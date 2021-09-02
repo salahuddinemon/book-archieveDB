a4f9f7b2bbc4dae1ff28deb00c13518ecb26f8e6
salahuddinemon <emonthex@gmail.com>
2021-09-02 23:29:57 +0600

    commit

--- app.js
+++ app.js
@@ -1,35 +1,51 @@
-// Load Data from API 
+//call sms handling & style 
+const emptyError = document.getElementById('empty-value');
+// Load Data from API
 const loadOpenLibrary = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value
-    ////clear search input
+    //clear search input
     searchField.value = '';
-    // data fetch
-    fetch(`https://openlibrary.org/search.json?q=${searchText}`)
-        .then(res => res.json())
-        .then(data => displayOpenLibrary(data.docs))
+    //handle empty search
+    if (searchText === '') {
+        emptyError.innerText = 'Search field can not be empty';
+    }
+    else {
+        fetch(`https://openlibrary.org/search.json?q=${searchText}`)
+            .then(res => res.json())
+            .then(data => displayOpenLibrary(data))
+    }
 };
-// loadOpenLibrary()
+
 // Display data into UI
-const displayOpenLibrary = docs => {
-    // console.log(docs);
+const displayOpenLibrary = data => {
+    const dataDocs = data.docs;
+    //show total number of results
+    const totalResults = document.getElementById('total-results');
+    totalResults.innerText = `Total Results Found: ${data.numFound}`;
+    totalResults.style.backgroundColor = 'Purple';
+    //display search result
     const searchResult = document.getElementById('search-result');
     // clear search results
     searchResult.textContent = '';
-    // data catching
-    docs.forEach(doc => {
-        const coverUrl = `https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg`
-        console.log(coverUrl)
-        // console.log(doc.author_name['0']);
+    emptyError.innerText = '';
+    if (searchResult === '') {
+        emptyError.innerText = 'Search field can not be empty';
+    };
+
+    // image data catching
+    dataDocs.forEach(dataDoc => {
+        const coverUrl = `https://covers.openlibrary.org/b/id/${dataDoc.cover_i}-M.jpg`
         const div = document.createElement('div');
+        // console.log(doc.author_name);
         div.innerHTML = `
         <div class="card h-100">
             <img src="${coverUrl}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h4 class="card-text">Books Name: ${doc.title} ${doc.text.length[4]}</h4>
-                <h6 class="card-title">Author Name: ${doc.author_name}</h6>
-                <p class="card-text">First Publish: ${doc.first_publish_year}</p>
-                <p class="card-text">Publisher: ${doc.publisher}</p>
+                <h4 class="card-text">Book Name: ${dataDoc.title}</h4>
+                <h6 class="card-title">Author Name: ${dataDoc.author_name}</h6>
+                <p class="card-text">First Publish: ${dataDoc.first_publish_year}</p>
+                <p class="card-text">Publisher: ${dataDoc.publisher}</p>
              </div >
     <div class="card-footer">
         <small class="text-muted"></small>
